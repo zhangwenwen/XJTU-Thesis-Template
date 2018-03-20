259253e3774c0b5af441fb373b0519d0a760ac51
zhangwenwen <zhangwenwen1000@gmail.com>
2018-03-20 12:13:56 +0800

    compile in OSX
    1.compile the template in OSX ,with install the missing fonts:SimSun and SimHei.
    
    2.create  clean.sh for unix platform

---
+++ clean.sh
@@ -0,0 +1,29 @@
+rm *.aux
+rm *.bak
+rm *.log
+
+rm *.bbl
+rm *.dvi
+rm *.blg
+rm *.thm
+rm *.toc
+rm *.toe
+
+rm *.lof 
+rm *.lot
+rm *.out 
+
+rm *.fen
+rm *.ten
+rm *.ps
+
+rm *.gz
+rm *.gz\(busy\)
+rm *.synctex
+rm *.synctex\(busy\)
+
+rm *.loa
+
+rm setup/*.aux
+rm body/*.aux
+rm appendix/*.aux
